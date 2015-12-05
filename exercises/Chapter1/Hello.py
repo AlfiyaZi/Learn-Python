b69d14be0472a6247b09b288b17719ee45dc0225
Jeremy Morgan <jeremymorgan@gmail.com>
2015-12-04 17:07:11 -0800

    Adding Image

---
+++ exercises/Chapter1/Hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+b = "test"
+a = b
+print("A is " + a)
+print("B is " + b)
+
+a = "test2"
+
+print("A is " + a)
+print(type(b))
+
+
+
+
---
+++ part1/0-repl.md
@@ -0,0 +1,3 @@
+##What on Earth is a REPL?
+
+![ ](/images/chapter1-01.jpg  "k")
